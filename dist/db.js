22d9fb4415c96907ea21845f5dbf545a97679496
bhoopendra <bhoopendrashukla3@gmail.com>
2025-02-28 13:55:21 +0530

    changes

--- dist/db.js
+++ dist/db.js
@@ -6,12 +6,12 @@ require("reflect-metadata");
 const entities_1 = require("./entities");
 exports.AppDataSource = new typeorm_1.DataSource({
     type: "mysql",
-    host: "localhost", // Ensure MySQL is running on localhost and the port is correct.
-    port: 3306, // Default MySQL port
-    username: "root", // Make sure this is the correct username for your MySQL user
-    password: "root", // Update with your MySQL password if necessary
-    database: "users", // Ensure that the "users" database exists in MySQL
-    entities: [entities_1.User, entities_1.Product], // Ensure these entities are correctly defined
-    synchronize: true, // For auto-syncing the schema (be careful with this in production)
-    logging: true, // Enable logging to see queries and potential issues
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "root",
+    database: "users",
+    entities: [entities_1.User, entities_1.Product],
+    synchronize: true,
+    logging: true,
 });
