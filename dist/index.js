3925a04e3f5e8171809d11d0d03b94d6a04b5780
bhoopendra <bhoopendrashukla3@gmail.com>
2025-02-28 14:37:12 +0530

    add the changes

--- dist/index.js
+++ dist/index.js
@@ -22,7 +22,7 @@ app.use("/api/v1", authRouter_1.default);
 app.use(errorhandllingMiddleware_1.default);
 db_1.AppDataSource.initialize()
     .then(() => console.log("database connected"))
-    .catch((err) => console.log("dayabase connection error", err));
+    .catch((err) => console.log("database connection error", err));
 io.on("connection", (socket) => {
     console.log("New client connected:", socket.id);
     socket.on("sendMessage", (data) => {
